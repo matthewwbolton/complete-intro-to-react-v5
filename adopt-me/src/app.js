35e5ccc398f87f6d019b052de462b41800ff1110
Matthew Bolton <matthewwbolton@gmail.com>
2020-12-31 11:37:26 -0500

    installed Reach Router to navigate to and from the Details page back to the index route

--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   //   return React.createElement("div", {}, [
@@ -25,11 +27,19 @@ const App = () => {
   return (
     <React.StrictMode>
       <div>
-        <h1 id="something important">Adopt Me!</h1>
+        <header>
+          <Link to="/" id="something-important">
+            Adopt Me!
+          </Link>
+        </header>
+        {/* <h1 id="something important">Adopt Me!</h1> */}
         {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
       <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
       <Pet name="Doink" animal="Cat" breed="Mix" /> */}
-        <SearchParams />
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
